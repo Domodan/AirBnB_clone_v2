4e00cf0419f74c8ede329486e28d799ea4891be7
domodan <ongomdaniel9@gmail.com>
2023-10-13 09:40:36 -0700

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,30 +11,31 @@ from os import path
 env.hosts = ['100.25.201.83', '52.3.249.62']
 env.user = 'ubuntu'
 
-
-@task(alias="deploy")
-def do_deploy(archive_path) -> bool:
-    """
-    Deploy the application to the web servers
-    Args:
-        None
-    Returns:
-        True ( if all goes well)
-        False (if something is not right)
-    """
-    if not path.exists(archive_path):
+def do_deploy(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
         return False
     try:
-        arc = archive_path.split("/")
-        base_loc = arc[1].strip('.tgz')
-        put(archive_path, '/tmp/')
-        sudo('mkdir -p /data/web_static/releases/{}'.format(base_loc))
-        main_loc = "/data/web_static/releases/{}".format(base_loc)
-        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main_loc))
-        sudo('rm /tmp/{}'.format(arc[1]))
-        sudo('mv {}/web_static/* {}/'.format(main_loc, main_loc))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}/ "/data/web_static/current"'.format(main_loc))
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
-    except Exception:
+    except:
         return False
