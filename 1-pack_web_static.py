9eda42dbb61b19c19b5f367edbe3f6309b117d7a
domodan <ongomdaniel9@gmail.com>
2023-10-13 09:04:47 -0700

    compress be sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
+Compress the files in web_static to create
+an archive, for deployment
+Usage: ./1-pack_web_static.py do_pack
 """
+
 from fabric.api import local
 from datetime import datetime
 
 
-def do_pack():
-    """Function to compress directory
-
-    Return: path to archive on success; None on fail
+def do_pack() -> None:
     """
-    # Get current time
-    now = datetime.now()
-    now = now.strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + now + '.tgz'
-
-    # Create archive
+    Pack the files to an archive for deployment
+    Args:
+        None
+    Returns:
+        Path(path of the archive) - if true
+        None ( if any exceptions occur)
+    """
+    c_time = datetime.now()
+    arch_name = "web_static_{}.tgz".format(c_time.strftime("%Y%m%d%H%M%S"))
     local('mkdir -p versions/')
-    result = local('tar -cvzf {} web_static/'.format(archive_path))
-
-    # Check if archiving was successful
-    if result.succeeded:
-        return archive_path
-    return None
+    create = local('tar -cvzf versions/{} web_static'.format(arch_name))
+    if create is not None:
+        return arch_name
+    else:
+        return None
