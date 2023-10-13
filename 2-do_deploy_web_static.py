a0b0f37b62f1fdcb0b1101cd08baa8637de43c78
domodan <ongomdaniel9@gmail.com>
2023-10-13 10:02:38 -0700

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-
 """
-Deploy the to a remote server
-Usage: ./2_do_deploy_web_static.py do_deploy
+Fabric script to distribute an archive to web servers and deploy it
 """
 
-from fabric.api import env, task, put, run, sudo
+from fabric.api import env, put, run, local
 from os import path
 
 env.hosts = ['100.25.201.83', '52.3.249.62']
 env.user = 'ubuntu'
+# env.key_filename = '<path_to_your_ssh_key>'
 
 
 def do_deploy(archive_path):
-    """ Deploys """
     if not os.path.exists(archive_path):
         return False
-    try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
 
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
+    archive_name = os.path.basename(archive_path)
+    archive_root = archive_name.replace(".tgz", "")
+    remote_path = "/tmp/{}".format(archive_name)
 
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
+    # Upload archive to /tmp/ on web servers
+    put(archive_path, remote_path)
 
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        return False
+    # Create folder for new version and uncompress the archive
+    run("mkdir -p /data/web_static/releases/{}/".format(archive_root))
+    run("tar -xzf {} -C /data/web_static/releases/{}/"
+        .format(remote_path, archive_root))
+
+    # Delete the archive
+    run("rm {}".format(remote_path))
+
+    # Delete the symbolic link current and create a new one
+    current_path = "/data/web_static/current"
+    run("rm -f {}".format(current_path))
+    run("ln -s /data/web_static/releases/{}/ {}".format(archive_root))
+
+    return True
