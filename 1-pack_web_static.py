05adaff0dc179dbd97b4c88199ae750595eb5bba
domodan <ongomdaniel9@gmail.com>
2023-10-13 09:37:46 -0700

    compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""
-Compress the files in web_static to create
-an archive, for deployment
-Usage: ./1-pack_web_static.py do_pack
-"""
-
-from fabric.api import local
+""" Function that compress a folder """
 from datetime import datetime
+from fabric.api import local
+import os
 
 
-def do_pack() -> None:
-    """
-    Pack the files to an archive for deployment
-    Args:
-        None
-    Returns:
-        Path(path of the archive) - if true
-        None ( if any exceptions occur)
-    """
-    c_time = datetime.now()
-    arch_name = "web_static_{}.tgz".format(c_time.strftime("%Y%m%d%H%M%S"))
-    local('mkdir -p versions/')
-    create = local('tar -cvzf versions/{} web_static'.format(arch_name))
-    if create is not None:
-        return arch_name
-    else:
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
         return None
