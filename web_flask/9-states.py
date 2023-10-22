68b56fb5187c5cbc73136abfd11aef8c227b19df
domodan <ongomdaniel9@gmail.com>
2023-10-22 12:16:29 -0700

    States for the states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Starts a Flask web application on Port 5000
+starts a Flask web application
 """
 
 from flask import Flask, render_template
@@ -11,11 +11,8 @@ app = Flask(__name__)
 
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<state_id>', strict_slashes=False)
-def states_list():
-    """
-        Display a HTML page with the states and cities listed in
-        alphabetical order
-    """
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
     states = storage.all("State")
     if state_id is not None:
         state_id = 'State.' + state_id
@@ -24,11 +21,8 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-        Closes the storage on teardown
-    """
+    """closes the storage on teardown"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
