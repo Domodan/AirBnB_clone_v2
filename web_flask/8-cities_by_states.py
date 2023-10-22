31ed0cf87018658b5c9e0f4f910b5486db95209b
domodan <ongomdaniel9@gmail.com>
2023-10-22 12:11:34 -0700

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Starts a Flask web application on Port 5000
+starts a Flask web application
 """
 
 from flask import Flask, render_template
@@ -10,20 +10,15 @@ app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def states_list():
-    """
-        Display a HTML page with the states and cities listed in
-        alphabetical order
-    """
-    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-        Closes the storage on teardown
-    """
+    """closes the storage on teardown"""
     storage.close()
 
 if __name__ == '__main__':
